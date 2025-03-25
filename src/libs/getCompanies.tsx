1fff99092e3f6a71214de2db288d49423c4d6eb6
Teetat Juirnarongrit <butsoap316@gmail.com>
2025-03-25 20:26:51 +0700

    no cache

--- src/libs/getCompanies.tsx
+++ src/libs/getCompanies.tsx
@@ -1,6 +1,8 @@
+import { cache } from "react";
+
 export default async function getCompanies(){
     await new Promise((resolve) => setTimeout(resolve, 500));
-    const response = await fetch ("https://backend-gilt-five-89.vercel.app/api/v1/companies")
+    const response = await fetch ("https://backend-gilt-five-89.vercel.app/api/v1/companies",{cache:"no-store"})
     if(!response.ok){
         throw new Error("Failed to fetch companies")
     }
